a7baada94ea19b53632dd8148aa0ca52da26e654
tel yoshioka <fukurou_tori@hotmail.com>
2026-08-01 05:33:53 +0900

    TableLayoutPanel1のRow1（Label1の行）を黒で描画
    ## 変更内容
    
    ### MainPlayerForm.vb
    - TableLayoutPanel1_CellPaintでRow 1（Label1が配置されている行）を黒(RGB 0,0,0)で塗りつぶし

--- src/MainPlayerForm.vb
+++ src/MainPlayerForm.vb
@@ -1947,9 +1947,10 @@ Public Class MainPlayerForm
         Next
 
         ' Row 1
+        Dim blackBrush As New SolidBrush(Color.FromArgb(0, 0, 0))
         For col = 0 To 20
             If e.Column = col AndAlso e.Row = 1 Then
-                e.Graphics.FillRectangle(darkBrush2, e.CellBounds)
+                e.Graphics.FillRectangle(blackBrush, e.CellBounds)
             End If
         Next
 
